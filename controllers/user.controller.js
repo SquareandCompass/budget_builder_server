6a633753bd61ae0743188c2082630060b2c45a12
Eric Winebrenner <ejwinebrenner@gmail.com>
2023-02-08 11:36:36 -0500

    User / Admin roles configured. Update / Delete User established

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -22,7 +22,7 @@ router.post('/signup', async (req, res) => {
 
         const newUser = user.save();
 
-        const token = jwt.sign({id: newUser._id}, JWT, expires);
+        const token = jwt.sign({id: (await newUser)._id}, JWT, expires);
 
         const displayObj = {
             id: (await newUser)._id,
@@ -45,14 +45,16 @@ router.post('/login', async (req, res) => {
         
         const { email, password } = req.body;
 
+        console.log("Password: ", password);
+
         const user = await User.findOne({email: email});
-        const passwordCheck = bcrypt.compare(password, user.password);
+        const passwordCheck = await bcrypt.compare(password, user.password);
 
         if(!user || !passwordCheck) {
             throw new Error("Email or Password do not match our records.")
         }
 
-        const token = jwt.sign({id: user._id}, JWT, expires);
+        const token = jwt.sign({id: (await user)._id}, JWT, expires);
 
         const displayObj = {
             id: (await user)._id,
@@ -69,29 +71,54 @@ router.post('/login', async (req, res) => {
     }
 });
 
+//TODO: Consider how to update User Connection.
 router.patch('/profile', validate, async (req, res) => {
     try {
 
-        /* 
-            Need to consider what is getting updated.
-                - name, email, password, userConnections, role
-        */
-
-        const userId = req.user.id;
-        const info = req.body;
+        const user = req.user;
+        const { 
+            name, email, oldPassword, newPassword, newPasswordCheck, role 
+        } = req.body;
         const returnOption = {new: true};
 
-        const user = await User.findOneAndUpdate({_id: userId}, info, returnOption);
+        console.log("Updated: ", oldPassword, newPassword)
 
-        const displayObj = {
-            id: (await user)._id,
-            name: (await user).fullName,
-            email: (await user).email,
-            connections: (await user).userConnections,
-            no_connections: (await user).userConnections.length
+        let updatedPassword;
+        let updatedRole;
+
+        if(newPassword === newPasswordCheck) {
+
+            const verifyPass = bcrypt.compare(oldPassword, user.password);
+
+            if(verifyPass) {
+                updatedPassword = bcrypt.hashSync(newPassword, 13);
+            }
         }
 
-        user ?
+        if(role) {
+            if(user.role !== "admin") throw new Error("User not authroized to alter the role");
+
+            updatedRole = role;
+        }
+
+        const info = {
+            name, email, 
+            password: updatedPassword,
+            role: role ? updatedRole : "user"
+        }
+
+        const updateUser = await User.findOneAndUpdate({_id: user.id}, info, returnOption);
+
+        const displayObj = {
+            id: (await updateUser)._id,
+            name: (await updateUser).fullName,
+            email: (await updateUser).email,
+            password: (await updateUser).password,
+            connections: (await updateUser).userConnections,
+            no_connections: (await updateUser).userConnections.length
+        }
+
+        updateUser ?
             success(res, displayObj) :
             issue(res);
         
@@ -100,21 +127,80 @@ router.patch('/profile', validate, async (req, res) => {
     }
 });
 
-//* Admin Create
-// router.post('/create/admin', async(req,res) => {
+//* User Connection 
 
-// });
+//* Connection Array Updates
+
+//* Admin Create
+router.post('/create/admin', async(req,res) => {
+    try {
+        
+        const { 
+            first, last, email, password, passwordCheck 
+        } = req.body;
+
+        if(password !== passwordCheck) throw new Error("Password doesn't match Check");
+
+        const admin = new User({
+            first, last, email, 
+            password: bcrypt.hashSync(password, 13),
+            role: "admin"
+        });
+
+        const newAdmin = admin.save();
+
+        const token = jwt.sign({id: (await newAdmin)._id}, JWT, expires);
+
+        const displayObj = {
+            id: (await newAdmin)._id,
+            name: (await newAdmin).fullName,
+            email: (await newAdmin).email,
+            role: (await newAdmin).role,
+            token: token
+        }
+
+        newAdmin ?
+            success(res, displayObj) :
+            issue(res);
+
+    } catch (err) {
+        error(res, err);
+    }
+});
 //* Admin Update
 // router.put('/:id', async(req, res) => {
 
 // });
 
-// router.delete('/:id', async (req, res) => {
-//     try {
+//* Get All Users : Admin Route
+
+router.delete('/:id', validate, async (req, res) => {
+    try {
         
-//     } catch (err) {
-//         error(res, err);
-//     }
-// });
+        const { id } = req.params;
+        const user = await User.findById({_id: id});
+        if(!user) throw new Error('No User Exists');
+
+        const token_id = req.user.id;
+
+        if( user.equals(token_id) || req.user.role === "admin") {
+            const deleteUser = await User.findByIdAndDelete(id)
+            console.log('Deleted User: ', deleteUser);
+
+            const displayObj = {
+                status: "User Deleted",
+                name: (await deleteUser).fullName,
+                email: (await deleteUser).email
+            }
+
+            deleteUser ?
+                success(res, displayObj) :
+                issue(res);
+        }
+
+    } catch (err) {
+        error(res, err);
+    }
+});
 
 module.exports = router;
--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,5 +1,6 @@
 const { User } = require('../models');
 const { error } = require('../helpers');
+const jwt = require('jsonwebtoken');
 const JWT = process.env.JWT;
 
 const validateSession = async (req, res, next) => {
@@ -9,7 +10,10 @@ const validateSession = async (req, res, next) => {
         const decoded = jwt.verify(token, JWT);
         if(!decoded) throw new Error('Not Authorized');
 
-        const user = await User.findOne({_id: token.id});
+        console.log(decoded);
+
+        const user = await User.findOne({_id: decoded.id});
+
         if(!user) throw new Error('User not in Database');
 
         req.user = user;
